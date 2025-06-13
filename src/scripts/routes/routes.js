f4f4d0953665320c79af88648f5ae38b0f1d8181
Fauzy169 <ahmadfauzy169@gmail.com>
2025-06-13 23:14:53 +0800

    Imported notfound

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -5,6 +5,7 @@ import StoryDetailPage from '../pages/story/detail-page';
 import AddStoryPage from '../pages/add-story/add-story-page';
 import LoginPage from '../pages/auth/login-page';
 import RegisterPage from '../pages/auth/register-page';
+import NotFoundPage from '../pages/not-found/not-found-page';
 
 const routes = {
   '/': new HomePage(),
